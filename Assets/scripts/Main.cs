be303e61a4b092d48c268bc6871fa8fd64109aed
sehm <sehmlj@gmail.com>
2013-06-24 20:10:12 +0900

    ダンジョン生成仮実装 + 確確認のための簡易表示を実装

---
+++ Assets/scripts/Main.cs
@@ -0,0 +1,115 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using Dungeion;
+
+public class Main : MonoBehaviour
+{
+
+    void Start ()
+    {
+        m_map = new DungeionMap();
+        m_map.Generate();
+
+        this.InitCamera();
+        this.InitGridDisp();
+    }
+
+    /// 初期化：グリッド表示
+    private void InitGridDisp()
+    {
+        m_dispGridList = new GameObject[DungeionMap.AreaLineNum,DungeionMap.AreaRowNum];
+
+        m_gridRoot = new GameObject("Grid");
+        m_gridRoot.transform.position = Vector3.zero;
+
+        for(int y = 0; y < m_map.AreaList.GetLength(0); y++){
+            for(int x = 0; x < m_map.AreaList.GetLength(1); x++){
+                m_dispGridList[y,x] = this.CreateCube(x,y);
+            }
+        }
+        for(int y = 0; y < m_map.AreaList.GetLength(0); y++){
+            for(int x = 0; x < m_map.AreaList.GetLength(1); x++){
+                this.InitDispArea(x,y);
+            }
+        }
+    }
+    private GameObject CreateCube(int x,int y)
+    {
+        var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        var trans = go.transform;
+        trans.parent = m_gridRoot.transform;
+        trans.localPosition = new Vector3(0f + GridWidth * x, 0f - GridHeight * y, 0f);
+        trans.localScale    = new Vector3(10f,10f,10f);
+        return go;
+    }
+
+    /// 初期化：エリア表示
+    private void InitDispArea(int x,int y)
+    {
+        var info = m_map.AreaList[y,x];
+        var go   = m_dispGridList[y,x];
+
+        Color col = Color.white;
+        switch(info.Type){
+            case MapAreaType.None: col = Color.gray;  break;
+            case MapAreaType.Room: col = Color.green; break;
+            case MapAreaType.Road: col = Color.white; break;
+        }
+        go.renderer.material.color = col;
+
+        // つながっているエリア同士のとこに線を引く
+        if( info.ConnectDir[(int)MapAreaTypeDir.Upper] != null ){
+            this.DrawLine(go.transform.position, m_dispGridList[y - 1,x].transform.position);
+        }
+        if( info.ConnectDir[(int)MapAreaTypeDir.Bottom] != null ){
+            this.DrawLine(go.transform.position, m_dispGridList[y + 1,x].transform.position);
+        }
+        if( info.ConnectDir[(int)MapAreaTypeDir.Left] != null ){
+            this.DrawLine(go.transform.position, m_dispGridList[y,x - 1].transform.position);
+        }
+        if( info.ConnectDir[(int)MapAreaTypeDir.Right] != null ){
+            this.DrawLine(go.transform.position, m_dispGridList[y,x + 1].transform.position);
+        }
+    }
+    private GameObject DrawLine(Vector3 startPos,Vector3 endPos)
+    {
+        var go = new GameObject("Line");
+
+        var trans = go.transform;
+        trans.parent = m_gridRoot.transform;
+        trans.position = Vector3.zero;
+
+        var lineRenderer = go.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material (Shader.Find("Particles/Additive"));
+        lineRenderer.SetWidth(2,2);
+
+        lineRenderer.SetVertexCount(2);
+        lineRenderer.SetPosition(0, startPos);
+        lineRenderer.SetPosition(1, endPos);
+
+        return go;
+    }
+
+
+    /// カメラ設定
+    private void InitCamera()
+    {
+        var cam = Camera.mainCamera;
+
+        cam.transform.position = new Vector3(GridWidth * DungeionMap.AreaRowNum / 2,
+                                             -1f * (GridHeight * DungeionMap.AreaLineNum / 2f),
+                                             -40f);
+        cam.orthographicSize = GridHeight * DungeionMap.AreaLineNum;
+    }
+
+
+    private const float GridWidth  = 20f;
+    private const float GridHeight = 20f;
+
+    private DungeionMap      m_map;
+    private GameObject[,]    m_dispGridList;
+
+    private GameObject       m_gridRoot;
+}
---
+++ Assets/scripts/_Dungeon/DungeionMap.cs
@@ -0,0 +1,167 @@
+﻿
+using UnityEngine;
+using System;
+using System.Collections;
+
+/*
+    アルゴリズムの考え方は  http://www5f.biglobe.ne.jp/~kenmo/program/dangeon2/dangeon2.html
+*/
+
+namespace Dungeion
+{
+    /// <summary>
+    /// クラス：ダンジョン
+    /// </summary>
+    public class DungeionMap
+    {
+        public const int     AreaRowNum  = 6;
+        public const int     AreaLineNum = 6;
+
+        public MapArea[,]    AreaList;
+
+
+        /// <summary>
+        /// ダンジョン生成
+        /// </summary>
+        public void Generate()
+        {
+            this.AreaList = new MapArea[AreaLineNum,AreaRowNum];
+
+            // エリアの種類を決定
+            for(int y = 0; y < this.AreaList.GetLength(0); y++){
+                for(int x = 0; x < this.AreaList.GetLength(1); x++){
+                    this.CalcAreaType(x,y);
+                }
+            }
+
+            // エリア間のつながりを決定
+            for(int y = 0; y < this.AreaList.GetLength(0); y++){
+                for(int x = 0; x < this.AreaList.GetLength(1); x++){
+                    this.CalcAreaConnection(x,y);
+                }
+            }
+            m_nowBlockID = 0;
+            for(int y = 0; y < this.AreaList.GetLength(0); y++){
+                for(int x = 0; x < this.AreaList.GetLength(1); x++){
+                    this.CalcAreaBlockID(x,y);
+                }
+            }
+            // TODO : ブロックをつなげる
+
+            Debug.Log("[DungeionMap] Generated!!");
+        }
+        private void CalcAreaType(int x,int y)
+        {
+            int typeNum = Enum.GetNames(typeof(MapAreaType)).Length;
+
+            var info = new MapArea();
+            info.Type = (MapAreaType)m_random.Next(typeNum); // TODO : 各種類ごとに確率いるかもね。
+            this.AreaList[y,x] = info;
+        }
+
+        private void CalcAreaConnection(int x,int y)
+        {
+            MapArea info = this.AreaList[y,x];
+            if( info.Type == MapAreaType.None ){
+                return;
+            }
+
+            // Upper
+            MapArea connectArea;
+            if( m_random.Next(3) < 1
+                && y > 0 ){
+                connectArea = this.AreaList[y - 1,x];
+                if( info.ConnectDir[(int)MapAreaTypeDir.Upper] == null
+                    && connectArea.Type != MapAreaType.None ){
+                    info.ConnectDir[(int)MapAreaTypeDir.Upper]         = connectArea;
+                    connectArea.ConnectDir[(int)MapAreaTypeDir.Bottom] = info;
+                }
+            }
+            // Bottom
+            if( m_random.Next(3) < 1
+                && y < this.AreaList.GetLength(0) - 1 ){
+                connectArea = this.AreaList[y + 1,x];
+                if( info.ConnectDir[(int)MapAreaTypeDir.Bottom] == null
+                    && connectArea.Type != MapAreaType.None ){
+                    info.ConnectDir[(int)MapAreaTypeDir.Bottom]       = connectArea;
+                    connectArea.ConnectDir[(int)MapAreaTypeDir.Upper] = info;
+                }
+            }
+            // Left
+            if( m_random.Next(3) < 1
+                && x > 0 ){
+                connectArea = this.AreaList[y,x - 1];
+                if( info.ConnectDir[(int)MapAreaTypeDir.Left] == null
+                    && connectArea.Type != MapAreaType.None ){
+                    info.ConnectDir[(int)MapAreaTypeDir.Left]         = connectArea;
+                    connectArea.ConnectDir[(int)MapAreaTypeDir.Right] = info;
+                }
+            }
+            // Right
+            if( m_random.Next(3) < 1
+                && x < this.AreaList.GetLength(1) - 1 ){
+                connectArea = this.AreaList[y,x + 1];
+                if( info.ConnectDir[(int)MapAreaTypeDir.Right] == null
+                    && connectArea.Type != MapAreaType.None ){
+                    info.ConnectDir[(int)MapAreaTypeDir.Right]       = connectArea;
+                    connectArea.ConnectDir[(int)MapAreaTypeDir.Left] = info;
+                }
+            }
+        }
+
+        /// 到達できるエリア群ごとに ID をつけてく.
+        private void CalcAreaBlockID(int x,int y)
+        {
+            MapArea info = this.AreaList[y,x];
+            if( info.Type != MapAreaType.Room
+                || info.BlockID >= 0 ){  // すでにブロック判定済み
+                return;
+            }
+
+            this.CalcAreaBlockID(info);
+            ++m_nowBlockID;
+        }
+        private void CalcAreaBlockID(MapArea info)
+        {
+            if( info.BlockID >= 0 ){  // すでにブロック判定済み
+                return;
+            }
+            info.BlockID = m_nowBlockID;
+
+            MapArea checkArea;
+            for(int i = 0; i < info.ConnectDir.Length; i++){
+                checkArea = info.ConnectDir[i];
+                if( checkArea != null ){
+                    this.CalcAreaBlockID(checkArea);
+                }
+            }
+        }
+
+
+        private int m_nowBlockID = 0;
+        private System.Random   m_random = new System.Random();
+    }
+
+
+
+    /// エリア種類
+    public enum MapAreaType
+    {
+        None,     ///< なし
+        Room,     ///< 部屋がある
+        Road,     ///< 通路だけ
+    }
+    public enum MapAreaTypeDir
+    {
+        Upper,
+        Bottom,
+        Left,
+        Right
+    }
+    public class MapArea
+    {
+        public MapAreaType  Type;
+        public MapArea[]    ConnectDir = new MapArea[4];
+        public int          BlockID = -1;
+    }
+}
